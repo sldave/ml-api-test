5301c4aba1cba97d79c73d9f37847535eef000c4
Dave S <publicdavman@gmail.com>
2016-10-18 11:50:28 -0400

    Styles and Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
+declare let accounting:any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	isDone:boolean;
+	data: any[];
+	ac = accounting;
+
+	constructor(public http:Http) {}
+
+	goSearch( element:HTMLInputElement ):void {
+		if(element.value != "") {
+      this.isDone = false;
+      this.http.request(`https://api.mercadolibre.com/sites/MLV/search?q=${element.value}`).subscribe((res: Response) => {
+        this.data = res.json();
+        console.log(this.data);
+        console.log(`This should be formated: ${this.ac.formatMoney(4999.99, "BsF", 2, ".", ",")}`);
+        this.isDone = true;
+      })
+    } else {
+      console.log(`Invalid value in ${element.name}.`);
+    }
+
+	}  
 }
